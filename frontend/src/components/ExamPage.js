ba2058859c5107f3f22f7a41bc1c2516931809af
R-Najdaoui <najdaoui.ra@gmail.com>
2025-12-02 22:43:11 +0100

    Set up React App with routing

---
+++ frontend/src/components/ExamPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExamPage = () => {
+  return (
+    <div>
+      <h2>Take the Exam</h2>
+      <p>Write your code below:</p>
+      <textarea rows="10" cols="50"></textarea>
+    </div>
+  );
+};
+
+export default ExamPage;
